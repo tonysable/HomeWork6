621312e64151a7390c0da245921e3d4f56be04f2
Sable <bandidos_150@mail.ru>
2022-10-20 20:37:19 +0300

    Задача 41

---
+++ Task41/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+        for(int i = 0; i < size; i++)
+        {
+            Console.WriteLine("Введите любое число: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+
+        }
+    return array;
+}
+
+int CountPos(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) count += 1;
+    }
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine($"]");
+}
+
+
+int[] porno = CreateArray(7);
+int sum = CountPos(porno);
+PrintArray(porno);
+Console.WriteLine($" -> {sum}");
